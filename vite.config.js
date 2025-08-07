08707edf09ad719ec88f494ad88b966a822b345d
Anton Pustovoi <apustovoi@agilevision.io>
2025-08-08 01:19:36 +0300

    Generate PWA assets

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,34 @@ export default defineConfig(() => {
       pigment(),
       mkcert(),
       tsconfigPaths(),
-      VitePWA({ registerType: "autoUpdate" }),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          icons: [
+            {
+              src: "pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+      }),
     ],
     build: {
       outDir: "docs",
