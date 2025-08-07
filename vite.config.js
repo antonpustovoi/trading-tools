42139874aea25ed0d61d6d3ead36317d57565e65
Anton Pustovoi <apustovoi@agilevision.io>
2025-08-08 00:46:48 +0300

    Make PWA app

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
+// eslint-disable-next-line import/order
 import { pigment } from "@pigment-css/vite-plugin";
 
 export default defineConfig(() => {
   return {
     base: "/trading-tools",
-    plugins: [react(), pigment(), mkcert(), tsconfigPaths()],
+    plugins: [
+      react(),
+      pigment(),
+      mkcert(),
+      tsconfigPaths(),
+      VitePWA({ registerType: "autoUpdate" }),
+    ],
     build: {
       outDir: "docs",
     },
